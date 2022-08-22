a00f1b2e182a496f4fed76526d61f25e026df59d
VitaliiKablukov <99354660+VitaliiKablukov@users.noreply.github.com>
2022-08-22 22:09:42 +0000

    Deploying to gh-pages from @ VitaliiKablukov/goit-js-hw-09@1164bff1706f1684305107eccf634cbe580e4738 🚀

---
+++ 01-color-switcher.50f9fbf3.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");e.setAttribute("disabled",!0);var n=null;t.addEventListener("click",(function(o){t.setAttribute("disabled",!0),e.removeAttribute("disabled"),n=setInterval((function(){r.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){clearInterval(n),t.removeAttribute("disabled"),e.setAttribute("disabled",!0)}))}();
+//# sourceMappingURL=01-color-switcher.50f9fbf3.js.map

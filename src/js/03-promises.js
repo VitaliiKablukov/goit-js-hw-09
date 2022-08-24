b760d7941b10440507f1ffb5ec175fd295b3fdc4
VitaliiKablukov <vetal.kablukov98@gmail.com>
2022-08-25 00:52:55 +0300

    end 3 task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
   } else {
-    // Reject
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
   }
+}
+const form = document.forms[0];
+form.addEventListener('submit', callAmountPromise);
+//
+function callAmountPromise(event) {
+  event.preventDefault();
+  const formAmount = event.target.elements.amount.value;
+  let formDelay = Number(event.target.elements.step.value);
+  let formFirstDelay = Number(event.target.elements.delay.value);
+  if (
+    formAmount &&
+    formDelay &&
+    formFirstDelay &&
+    formAmount > 0 &&
+    formDelay > 0 &&
+    formFirstDelay > 0
+  ) {
+    setTimeout(() => {
+      for (let i = 0; i < formAmount; i += 1) {
+        setTimeout(() => {
+          createPromise(i + 1, formFirstDelay);
+          formFirstDelay += formDelay;
+        }, formDelay * i);
+      }
+    }, formFirstDelay);
+  } else {
+    Notiflix.Notify.failure(
+      `❌ Заповніть усі рядки значеннями які більше за нуль`
+    );
+  }
+
+  form.reset();
 }

b7a7125be22529a54cd4c69df5f79dce7492eee4
VitaliiKablukov <vetal.kablukov98@gmail.com>
2022-08-25 01:17:13 +0300

    upgrade task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,11 +2,11 @@ import Notiflix from 'notiflix';
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  } else {
-    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-  }
+  return new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      resolve({ position: position, delay: delay });
+    } else reject({ position: position, delay: delay });
+  });
 }
 const form = document.forms[0];
 form.addEventListener('submit', callAmountPromise);
@@ -27,7 +27,7 @@ function callAmountPromise(event) {
     setTimeout(() => {
       for (let i = 0; i < formAmount; i += 1) {
         setTimeout(() => {
-          createPromise(i + 1, formFirstDelay);
+          upgradecreatePromise(i + 1, formFirstDelay);
           formFirstDelay += formDelay;
         }, formDelay * i);
       }
@@ -39,4 +39,13 @@ function callAmountPromise(event) {
   }
 
   form.reset();
+}
+function upgradecreatePromise(count, timeOut) {
+  createPromise(count, timeOut)
+    .then(({ position, delay }) => {
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
 }
